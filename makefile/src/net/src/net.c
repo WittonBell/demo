9180d32c69134eb2b32efca1fb40c36f23505988
witton <witton@163.com>
2025-04-08 15:47:33 +0800

    添加Makefile注释及event库

--- makefile/src/net/src/net.c
+++ makefile/src/net/src/net.c
@@ -2,8 +2,8 @@
 #include <event2/event.h>
 
 ENetCode startNet(netOption opt) {
-	//struct event_config* cfg = event_config_new();
-	//struct event_base* base = event_base_new_with_config(cfg);
+	struct event_config* cfg = event_config_new();
+	event_config_free(cfg);
 
 	return ENetCodeOK;
 }
