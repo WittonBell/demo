a3d499c3ef4e7ed873f790320ed5f2f32d99d3f0
witton <witton@163.com>
2025-04-08 11:08:53 +0800

    在编译时生成依赖文件，不再单独生成，减少编译次数。使用变量V控制显示命令行。

---
+++ makefile/src/prec.h
@@ -0,0 +1,16 @@
+#ifndef PREC_H
+#define PREC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <event2/event.h>
+#include <stdio.h>
+
+#include "net/net.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
