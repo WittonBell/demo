99e0b374cff72de073b7589b153aeda7d9c6c6de
witton <witton@163.com>
2025-04-10 16:11:31 +0800

    Makefile Demo添加launch.json和tasks.json

--- makefile/src/main.c
+++ makefile/src/main.c
@@ -1,10 +1,15 @@
-
+#ifdef _WIN32
+#include <windows.h>
+#endif
 #include <stdio.h>
 
 extern void asmFoo();
 
 int main()
 {
+#ifdef _WIN32
+    printf("CodePage:%d\n", GetConsoleOutputCP());
+#endif
     asmFoo();
     return 0;
 }
