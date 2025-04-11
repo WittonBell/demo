fc455baf37b9984b2ae40cc8ac9eef66b3dfadf2
witton <witton@163.com>
2025-04-11 23:02:23 +0800

    opt

--- makefile/src/main.c
+++ makefile/src/main.c
@@ -1,14 +1,13 @@
 #ifdef _WIN32
 #include <windows.h>
 #endif
-#include <stdio.h>
 
 extern void asmFoo();
 
 int main()
 {
 #ifdef _WIN32
-    printf("CodePage:%d\n", GetConsoleOutputCP());
+    SetConsoleOutputCP(65001);
 #endif
     asmFoo();
     return 0;
